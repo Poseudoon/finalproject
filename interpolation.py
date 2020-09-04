bdc165f8f3bd5f6774563d4e32397c5769c87b06
Henri Renzelmann <henrirpunkt@web.de>
2020-09-04 21:17:08 +0200

    Adding interpolation

---
+++ interpolation.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from scipy import interpolate
+import numpy as np
+
+
+def interpol(plotsize, potp, interpol):
+
+    xnew = np.linspace(plotsize[0], plotsize[1], num=plotsize[2])
+
+    if interpol == "polynomial":
+        x = potp[:, 0]
+        y = potp[:, 1]
+        pot = interpolate.interp1d(x, y)
+
+        ynew = pot(xnew)
+
+    elif interpol == "linear":
+
+        ynew = []
+        n = 1
+
+        for i in range(0, len(xnew)):
+
+            if xnew[i] > potp[n, 0]:
+                n += 1
+                if potp[n-1, 0] == potp[n, 0]:
+                    n += 1
+
+            x = potp[n-1:n+1, 0]
+
+            y = potp[n-1:n+1, 1]
+
+            pot = interpolate.interp1d(x, y)
+
+            ynew.append(pot(xnew[i]))
