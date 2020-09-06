8d8541b03d969b6eeecc95f7d49fbdd80b6488a6
Henri Renzelmann <henrirpunkt@web.de>
2020-09-06 19:26:35 +0200

    Optimizing interpolation and fixing polynomial interpolation

--- execute.py
+++ execute.py
@@ -1,2 +1,14 @@
 # -*- coding: utf-8 -*-
+import os.path
+import numpy as np
+import interpolation
 
+
+def main():
+    basedata = np.loadtxt(os.path.join(input("Please enter the to your schrodinger.inp-file: "), "schrodinger.inp"))
+
+    xnew, ynew = interpolation.interpol(basedata[1, ], basedata[5:, ], basedata[3, ])
+
+
+if __name__ == "__main__":
+    main()
