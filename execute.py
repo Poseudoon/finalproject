3bd6dd49f9598d39494d05c2db2ac96b9f7478b7
Henri Renzelmann <henrirpunkt@web.de>
2020-10-01 15:00:27 +0200

    Fixing file-input in execute.py

--- execute.py
+++ execute.py
@@ -7,10 +7,18 @@ import visualizer
 
 def main():
     path = input("Please enter the to your schrodinger.inp-file: ")
-    basedata = np.loadtxt(os.path.join(path, "schrodinger.inp"))
+    fp = open(os.path.join(path, "schrodinger.inp"), "r")
+    basedata = fp.read().split()
+    fp.close()
     newpath = input("Please enter the path where to save the results: ")
 
-    xnew, ynew = interpolation.interpolating(basedata[1, ], basedata[5:, ], basedata[3, ], newpath)
+    for i in basedata:
+        try:
+            float(basedata[i])
+        except ValueError:
+            continue
+
+    xnew, ynew = interpolation.interpolating(basedata[1:4], basedata[8:], basedata[6], newpath)
 
     visualizer.visualise(xnew, ynew, newpath)
 
--- test_potsolver.py
+++ test_potsolver.py
@@ -4,4 +4,8 @@ import numpy as np
 import filemanager
 
 
+def test_1():
 
+    xx_expected = np.array([0.666666666666667, 0.416666666666667, -0.5])
+    xx_gauss = solvers.gaussian_eliminate(aa, bb)
+    assert np.allclose(xx_gauss, xx_expected, 1e-10, 1e-10)
